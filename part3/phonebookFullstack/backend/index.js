f5c29ce13208827463f2727c30abe402d3ae6237
bwsrcurtis <bwsrcurtis@gmail.com>
2023-07-15 20:03:24 -0400

    Deployed frontend

--- part3/phonebookFullstack/backend/index.js
+++ part3/phonebookFullstack/backend/index.js
@@ -5,6 +5,7 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.static('build'))
 
 const morgan = require('morgan')
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
@@ -91,7 +92,7 @@ app.post('/api/persons', (request, response) => {
 	response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
